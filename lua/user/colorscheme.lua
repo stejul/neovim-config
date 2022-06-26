0f286f5469464692266b7dfba7b4a59927501408
stejul <stejul@rocketmail.com>
2022-06-26 16:30:15 +0200

    chore: update configuration

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,9 +3,7 @@ local colorscheme = "tokyonight"
 if string.match(colorscheme, "tokyonight") then
   vim.g.tokyonight_style = "night"
 end
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
